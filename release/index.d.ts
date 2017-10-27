b8ff857e5198cfd57387aa02f1b7967cc05d303a
sergot <filip@sergot.pl>
2017-10-28 00:05:32 +0200

    new release

--- release/index.d.ts
+++ release/index.d.ts
@@ -1,2 +1,3 @@
 export * from './ngx-smart-form.module';
 export * from './components';
+export * from './interfaces';
--- release/interfaces/index.d.ts
+++ release/interfaces/index.d.ts
@@ -1,3 +1,5 @@
 export * from './ngx-smart-form-settings';
 export * from './ngx-smart-form-input';
 export * from './ngx-smart-form-select-data';
+export * from './ngx-smart-form-select-config';
+export * from './ngx-smart-form-config';
